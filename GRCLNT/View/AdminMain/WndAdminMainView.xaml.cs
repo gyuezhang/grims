2150e686d6337ab5be6b3e3096284a469fb86171
gyuezhang <gyuezhang@163.com>
2020-07-07 21:14:47 +0800

    GRCLNT-添加了文件夹组织，三个主窗口，和登录附页

--- GRCLNT/View/AdminMain/WndAdminMainView.xaml.cs
+++ GRCLNT/View/AdminMain/WndAdminMainView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GRCLNT
+namespace GRCLNT.View.AdminMain
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// WndAdminMainView.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WndAdminMainView : Window
     {
-        public MainWindow()
+        public WndAdminMainView()
         {
             InitializeComponent();
         }
