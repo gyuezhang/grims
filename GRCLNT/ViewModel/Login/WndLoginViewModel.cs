4a6cc65c8b81a33e24d66de9620d5aa686434418
gyuezhang <gyuezhang@163.com>
2020-07-16 17:04:13 +0800

    GRCLNT-adminlogin

--- GRCLNT/ViewModel/Login/WndLoginViewModel.cs
+++ GRCLNT/ViewModel/Login/WndLoginViewModel.cs
@@ -1,25 +1,64 @@
-﻿using MaterialDesignThemes.Wpf;
-using Stylet;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System;
 using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Threading;
+using Stylet;
+using MaterialDesignThemes.Wpf;
 
 namespace GRCLNT
 {
     public class WndLoginViewModel : Screen
     {
+        #region 初始化
+
         private IWindowManager _windowManager;
 
         public WndLoginViewModel(IWindowManager windowManager)
         {
             _windowManager = windowManager;
 
-            GRSocketHandler.ConnState += GRSocketHandler_ConnState;
-            GRSocket.Conn(cfgBd.SvrIp);
+            InitWidget();
+            TryToLink();
+            CheckAutoLogin();
         }
+
+        private void InitWidget()
+        {
+            if (cfgBd.RecordPwd)
+                curPwdBd = "11111111";
+            else
+                curPwdBd = "";
+        }
+
+        #endregion
+
+        #region 属性
+
+        public MCfg cfgBd { get; set; } = Cfg.Get();
+        public string linkStateTextBd { get; set; }
+        public Brush linkStateColorBd { get; set; }
+        public int pageIndexBd { get; set; } = 0;
+        public PackIconKind sysPackIconBd
+        {
+            get
+            {
+                return (pageIndexBd == 0) ? PackIconKind.Settings : PackIconKind.UndoVariant;
+            }
+            set
+            {
+                _ = (pageIndexBd == 0) ? PackIconKind.Settings : PackIconKind.UndoVariant;
+            }
+        }
+        public string curPwdBd { get; set; }
         public SnackbarMessageQueue messageQueueBd { get; set; } = new SnackbarMessageQueue(TimeSpan.FromSeconds(0.6));
+
+        private DispatcherTimer TimerLoginSuccess = new DispatcherTimer();
+        private static bool bFirstLogin { get; set; } = true;
+        private static int iPwdChangeCnt { get; set; } = 0;
+
+        #endregion
+
+        #region 委托
 
         private void GRSocketHandler_ConnState(ApiRes state)
         {
@@ -27,6 +66,7 @@ namespace GRCLNT
             {
                 case ApiRes.OK:
                     messageQueueBd.Enqueue("已成功连接到服务器");
+                    UpdateLinkState(1);
                     break;
                 case ApiRes.FAILED:
                     break;
@@ -35,22 +75,51 @@ namespace GRCLNT
             }
         }
 
-        //UI Logic
-        public int pageIndexBd { get; set; } = 0;
-        public string strName { get; set; }
-        public PackIconKind sysPackIconBd { 
-            get 
+        private void GRSocketHandler_adminLogin(ApiRes state)
+        {
+            GRSocketHandler.adminLogin -= GRSocketHandler_adminLogin;
+            switch (state)
             {
-                return (pageIndexBd == 0) ? PackIconKind.Settings : PackIconKind.UndoVariant;
-            }
-            set
-            {
-                _ = (pageIndexBd == 0) ? PackIconKind.Settings : PackIconKind.UndoVariant; 
+                case ApiRes.OK:
+                    ;
+                    AdminLoginSuccess();
+                    break;
+                case ApiRes.FAILED:
+                    messageQueueBd.Enqueue("登录失败，请确认用户名或密码正确");
+                    break;
+                default:
+                    break;
             }
         }
+
+        private void TimerLoginSuccess_Tick(object sender, System.EventArgs e)
+        {
+            messageQueueBd = new SnackbarMessageQueue(TimeSpan.FromSeconds(0.6));
+            TimerLoginSuccess.Stop();
+            ///注册服务器接口返回处理函数
+            GRSocketHandler.ConnState -= GRSocketHandler_ConnState;
+            App.Current.Dispatcher.Invoke((Action)(() =>
+            {
+                if (cfgBd.Name == "admin")
+                {
+                    var wndAdminMainViewModel = new WndAdminMainViewModel(_windowManager);
+                    this._windowManager.ShowWindow(wndAdminMainViewModel);
+                }
+                else
+                {
+                    var wndUserMainViewModel = new WndUserMainViewModel(_windowManager);
+                    this._windowManager.ShowWindow(wndUserMainViewModel);
+                }
+                this.RequestClose();
+            }));
+        }
+        #endregion
+
+        #region 方法
+
         public void SettingCmd()
         {
-            pageIndexBd = pageIndexBd == 1?0:1;
+            pageIndexBd = pageIndexBd == 1 ? 0 : 1;
         }
         public void BackCmd()
         {
@@ -60,47 +129,82 @@ namespace GRCLNT
         {
             this.RequestClose();
         }
-
-        //
         public void pwdChangedCmd()
         {
-
+            iPwdChangeCnt++;
         }
-
         public void LoginCmd()
         {
-            LoginSuccess();
-            if (strName == "admin")
-                OpenAdminMainViewModel();
-            else
-                OpenUserMainViewModel();
+            if (iPwdChangeCnt > 1)
+                cfgBd.Pwd = Enc.GetMd5Hash(curPwdBd);
+            DoLogin(cfgBd.Name, cfgBd.Pwd);
         }
-
         public void TestLinkCmd()
         {
-
+            TryToLink();
         }
-
-        //
-        public void OpenUserMainViewModel()
+        private void TryToLink()
         {
-            var wndUserMainViewModel = new WndUserMainViewModel(_windowManager);
-            this._windowManager.ShowWindow(wndUserMainViewModel);
-            this.RequestClose();
+            GRSocketHandler.ConnState += GRSocketHandler_ConnState;
+            GRSocket.Conn(cfgBd.SvrIp);
+            UpdateLinkState(0);
         }
-        public void OpenAdminMainViewModel()
+        private void CheckAutoLogin()
         {
-            var wndAdminMainViewModel = new WndAdminMainViewModel(_windowManager);
-            this._windowManager.ShowWindow(wndAdminMainViewModel);
-            this.RequestClose();
+            if (cfgBd.AutoLogin && bFirstLogin)
+                AutoLogin();
         }
-
-
-        public MCfg cfgBd { get; set; } = Cfg.Get();
-
-        public void LoginSuccess()
+        private void AutoLogin()
+        {
+            messageQueueBd = new SnackbarMessageQueue(TimeSpan.FromSeconds(5));
+            messageQueueBd.Enqueue("正在自动登录",
+                "取消",
+                CancelAutoLogin);
+            DoLogin(cfgBd.Name, cfgBd.Pwd);
+        }
+        private void CancelAutoLogin()
+        {
+            messageQueueBd = new SnackbarMessageQueue(TimeSpan.FromSeconds(0.6));
+            TimerLoginSuccess.Stop();
+            messageQueueBd.Enqueue("自动登录已取消");
+        }
+        void DoLogin(string name, string pwd)
+        {
+            if (name == "admin")
+            {
+                GRSocketHandler.adminLogin += GRSocketHandler_adminLogin;
+                GRSocketAPI.AdminLogin(pwd);
+            }
+            else
+                ;
+        }
+        private void AdminLoginSuccess()
         {
             Cfg.Set(cfgBd);
+
+            messageQueueBd.Enqueue("登录成功");
+            bFirstLogin = false;
+            TimerLoginSuccess.Interval = new TimeSpan(0, 0, 0, 3);
+
+            TimerLoginSuccess.Tick += TimerLoginSuccess_Tick;
+            TimerLoginSuccess.Start();
         }
+        private void UpdateLinkState(int state)
+        {
+            switch (state)
+            {
+                case 0:
+                    linkStateTextBd = "已断开";
+                    linkStateColorBd = Brushes.Red;
+                    break;
+                case 1:
+                    linkStateTextBd = "已连接";
+                    linkStateColorBd = Brushes.Green;
+                    break;
+                default:
+                    break;
+            }
+        }
+        #endregion
     }
 }
